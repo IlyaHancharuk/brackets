b4b4846025dc028c6fdfd3ec1fa6a0471993c905
Ilya Hancharuk <innot707@gmail.com>
2022-07-27 18:06:44 +0300

    13/20 pass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openBrackets = []
+    let pairBrackets = {}
+    let stack = []
+    
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      openBrackets.push(bracketsConfig[i][0])
+      pairBrackets[bracketsConfig[i][1]] = bracketsConfig[i][0]
+      }
+
+    for (let i = 0; i < str.length; i++) {
+        let curSymbol = str[i]
+
+        if (openBrackets.includes(curSymbol)) {
+          stack.push(curSymbol)
+          console.log(stack)
+        } else {
+          if (stack.length === 0) {
+            return false
+          }
+  
+          let topElem = stack[stack.length - 1]
+  
+          if (pairBrackets[curSymbol] === topElem) {
+            stack.pop()
+          } else {
+            return false;
+          }
+        }
+      }
+  
+      return stack.length === 0;
 }
